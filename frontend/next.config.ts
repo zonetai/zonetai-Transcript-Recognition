60ba187db7f5c60c6feab668acd8dc66dba0ca1d
Nody-Peng <ablecck12@gmail.com>
2026-07-22 11:42:47 +0800

    doc: vu. e931j4gj4jp4wu6

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: "/:path*",
         headers: [
           {
             key: "Content-Security-Policy",
